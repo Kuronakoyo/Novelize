f606766348a72aef2403800b856e924578ccc6e0
kuronakoyo <kurronakoyo@gmail.com>
2020-11-20 09:45:07 +0900

    add new files
    unity 2020年11月13日

---
+++ Assets/Scenes/ExGameManeger.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ExGameManeger : MonoBehaviour
+{
+    // 親のオブジェクト(Messagewindow)
+    [SerializeField]
+    private Transform parent = null;
+    //　文字の表示速度
+    [SerializeField]
+    private float fontstepTimer = 0.1f;
+
+    //一文字表示のためのプレハブ
+    [SerializeField]
+    private GameObject onceTextPrefab = null;
+    //表示開始座標
+    [SerializeField]
+    private Vector2 startPosition = new Vector2(-870, -450);
+    // 横の文字数
+    [SerializeField]
+    private int wideStringLengthMax = 30;
+    // 文字の間隔
+    [SerializeField]
+    private float widePadding = 56;
+    //行の間隔
+    [SerializeField]
+    private float heightPadding = 66;
+    // 先頭からの文字数
+    private int currentPosition = 0;
+
+    //表示するメッセージオブジェクトを保持するためのリスト配列
+    private List<GameObject> messageFonts = new List<GameObject>();
+    //表示するテキストデータ
+    private string scenarioText = "あいうえお書きくださいああああああああああああああああああああああ";
+    //　文字表示中フラグ
+    private bool isDisplayFont = false;
+    // 文字表示終了フラグ
+    private bool isDisplayEnd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isDisplayFont && !isDisplayEnd)
+        {
+            // 文字表示中モードに設定
+            isDisplayFont = true;
+            // 文字表示
+            StartCoroutine(FontDisp());
+        }
+        //　全ての表示が終わって、マウス右クリックしていた場合
+        if (Input.GetMouseButtonDown(1) && isDisplayFont && isDisplayEnd)
+        {
+            // 表示中モードを解除
+            isDisplayFont = false;
+            // すべての文字オブジェクトの重力を有効にする
+            messageFonts.ForEach(msg => msg.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic);
+            // 一定時間後にフェードアウトするように設定する
+            messageFonts.ForEach(msg => msg.GetComponent<OnceTextView>().FadeOutOneceText());
+
+        }
+    }
+    IEnumerator FontDisp()
+    {
+        //　メッセージの最後まで表示していなければ文字表示を行う
+        while (scenarioText.Length > currentPosition)
+        {
+            //　現在のポジションから一文字取り出す
+            var msg = scenarioText.Substring(currentPosition, 1);
+            // 既定の座標にテキストオブジェクトを生成して文字を表示する
+            SetFont(msg, GetDispPosition());
+            // 現在のポジションを更新する
+            currentPosition++;
+            // 一定時間を待つ
+            yield return new WaitForSeconds(fontstepTimer);
+        }
+        //　すべて表示終了フラグを立てる
+        isDisplayEnd = true;
+    }
+
+    /// <summary>
+    /// 文字生成及び表示処理
+    /// </summary>
+    /// <param name="msg"></param>
+    /// <param name="pos"></param>
+    private void SetFont(string msg, Vector2 pos)
+    {
+        var onceMessageFont = Instantiate(onceTextPrefab, parent);
+        onceMessageFont.GetComponent<RectTransform>().localPosition = pos;
+        onceMessageFont.GetComponent<Text>().text = msg;
+        onceMessageFont.GetComponent<OnceTextView>().FadeInOneceText();
+        messageFonts.Add(onceMessageFont);
+    }
+    private Vector2 GetDispPosition()
+    {
+        return new Vector2(startPosition.x + (currentPosition % wideStringLengthMax) * widePadding,
+            startPosition.y - (currentPosition / wideStringLengthMax) * heightPadding);
+    }
+}
---
+++ Assets/Scenes/OnceTextView.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnceTextView : MonoBehaviour
+{
+    [SerializeField]
+    private float FadeInTimer = 0.1f;
+    [SerializeField]
+    private float FadeOutTimer = 0.3f;
+    [SerializeField]
+    private CanvasGroup onceCanvasGroup = null;
+    // Start is called before the first frame update
+    
+    //<summary>
+    //</summary>
+    public void FadeInOneceText()
+    {
+        //透明度を0にする
+        onceCanvasGroup.alpha = 0.0f;
+        //フェードインの開始
+        StartCoroutine(FadeIn());
+    }
+    private IEnumerator FadeIn()
+    {
+        // オブジェクトの透明度が1（不透明）になるまで処理を行う
+        while (onceCanvasGroup.alpha < 1.0f)
+        {
+            //　所定時間をかけて透明度を加算する
+            onceCanvasGroup.alpha += (1.0f / FadeInTimer)*Time.deltaTime;
+            //透明度のMAXが1なのでそれを超えたら1になるように修正する
+
+            if (onceCanvasGroup.alpha >= 1.0f)
+                onceCanvasGroup.alpha = 1.0f;
+            //１フレーム待機する
+            yield return null;
+
+        }
+    }
+    public void FadeOutOneceText(float delayTime = 0.0f)
+    {
+        onceCanvasGroup.alpha = 0.0f;
+        StartCoroutine(FadeOut(delayTime));
+    }
+
+    private IEnumerator FadeOut(float delayTime)
+    {
+        //一定時間を待つ
+        yield return new WaitForSeconds(delayTime);
+        // オブジェクトの透明度が0（透明）になるまで処理を行う
+        while (onceCanvasGroup.alpha < 0.0f)
+        {
+            //　所定時間をかけて透明度を加算する
+            onceCanvasGroup.alpha -=( 1.0f / FadeOutTimer)*Time.deltaTime;
+            //透明度のMAXが1なのでそれを超えたら1になるように修正する
+
+            if (onceCanvasGroup.alpha <= 0.0f)
+                onceCanvasGroup.alpha = 0.0f;
+            //１フレーム待機する
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
